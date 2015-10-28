42d868d59a0728029767c8010dfa8357548500d5
Daniel Holmlund <agnathan@gmail.com>
2015-10-28 15:08:34 -0700

    fix config.son path

--- devices/lightSensor/index.js
+++ devices/lightSensor/index.js
@@ -21,7 +21,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-var config = require("config.json");
+var config = require("./config.json");
 
 // Require the MQTT connections
 var mqtt = require('mqtt');
